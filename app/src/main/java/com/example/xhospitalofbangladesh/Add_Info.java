3b5f1cc235be6a36e26658ce0a40ebd3b221e239
kmminhaz <kmminhaj.khan@gmail.com>
2020-10-16 12:05:51 +0600

    X-Hospital-of-Bangladesh

---
+++ app/src/main/java/com/example/xhospitalofbangladesh/Add_Info.java
@@ -0,0 +1,32 @@
+package com.example.xhospitalofbangladesh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Add_Info extends AppCompatActivity {
+
+    private EditText name,number,time;
+    private Button submit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__info);
+
+        name = findViewById(R.id.NameId);
+        number = findViewById(R.id.NumberId);
+        submit = findViewById(R.id.submitBtnId);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Add_Info.this,"Your Appointment has been taken",Toast.LENGTH_LONG);
+            }
+        });
+    }
+}
